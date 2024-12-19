ee4a7754687064ec47a79c50e01c31d0074d2360
Prabodh-Kumar-Mallick <mallickprabodhkumar@gmail.com>
2024-12-19 22:15:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function Night(){
+
+    let img = document.getElementById('img')
+    img.src="NIGHT.jpg"
+    
+}
+
+function Day(){
+
+    let img = document.getElementById('img')
+    img.src="DAY.jpg"
+}
